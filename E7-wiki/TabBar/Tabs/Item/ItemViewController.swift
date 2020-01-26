9c8afe61d0cc6abf2327111c61b9adb745289462
Sho <sho.weblife@gmail.com>
2020-01-27 01:02:25 +0900

    :sparkles: created item tabview

--- E7-wiki/TabBar/Tabs/Item/ItemViewController.swift
+++ E7-wiki/TabBar/Tabs/Item/ItemViewController.swift
@@ -8,34 +8,103 @@
 
 import UIKit
 import SnapKit
+import WebKit
 
 final class ItemViewController: UIViewController {
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        return tableView
-    }()
+
+    let wkWebView = WKWebView()
+
+    var backButton: UIButton!
+    var forwadButton: UIButton!
+    var targetUrl = "https://wiki.denfaminicogamer.jp/epicseven/catalyst"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
 
-        navigationItem.title = "Item"
-        view = tableView
+        navigationItem.title = "Wiki"
+        
+        wkWebView.frame = view.frame
+        wkWebView.navigationDelegate = self
+        wkWebView.uiDelegate = self
+
+        // スワイプでの「戻る・すすむ」を有効にする
+        wkWebView.allowsBackForwardNavigationGestures = true
+
+        let urlRequest = URLRequest(url:URL(string:targetUrl)!)
+        wkWebView.load(urlRequest)
+        view.addSubview(wkWebView)
+
+        createWebControlParts()
+    }
+
+    /// 戻る・すすむボタン作成
+    private func createWebControlParts() {
+
+        let buttonSize = CGSize(width:40,height:40)
+        let offseetUnderBottom:CGFloat = 200
+        let yPos = (UIScreen.main.bounds.height - offseetUnderBottom)
+        let buttonPadding:CGFloat = 10
+
+        let backButtonPos = CGPoint(x:buttonPadding, y:yPos)
+        let forwardButtonPos = CGPoint(x:(buttonPadding + buttonSize.width + buttonPadding), y:yPos)
+
+        backButton = UIButton(frame: CGRect(origin: backButtonPos, size: buttonSize))
+        forwadButton = UIButton(frame: CGRect(origin:forwardButtonPos, size:buttonSize))
+
+        backButton.setTitle("<", for: .normal)
+        backButton.setTitle("< ", for: .highlighted)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.layer.backgroundColor = UIColor.black.cgColor
+        backButton.layer.opacity = 0.6
+        backButton.layer.cornerRadius = 5.0
+        backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+        backButton.isHidden = true
+        view.addSubview(backButton)
+
+        forwadButton.setTitle(">", for: .normal)
+        forwadButton.setTitle(" >", for: .highlighted)
+        forwadButton.setTitleColor(.white, for: .normal)
+        forwadButton.layer.backgroundColor = UIColor.black.cgColor
+        forwadButton.layer.opacity = 0.6
+        forwadButton.layer.cornerRadius = 5.0
+        forwadButton.addTarget(self, action: #selector(goForward), for: .touchUpInside)
+        forwadButton.isHidden = true
+        view.addSubview(forwadButton)
+
+    }
+
+    @objc private func goBack() {
+        wkWebView.goBack()
+    }
+
+    @objc private func goForward() {
+        wkWebView.goForward()
     }
 
 }
 
-extension ItemViewController: UITableViewDelegate {
+extension ItemViewController: WKNavigationDelegate {
 
+    // ウェブのロード完了時に呼び出される
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+
+        backButton.isHidden = (webView.canGoBack) ? false : true
+        forwadButton.isHidden = (webView.canGoForward) ? false : true
+    }
 }
 
-extension ItemViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+// target=_blank対策
+extension ItemViewController: WKUIDelegate {
+
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration,
+                 for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+
+        return nil
     }
 
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
 }
